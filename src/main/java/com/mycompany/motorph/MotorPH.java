ec3928f5b858977c1a2e554ef8c82bb18c861e1b
biancaysobellem.secreto <biancaysobellem.secreto@localhost>
2026-03-17 01:11:13 +0800

    Updated to adding my name Bianca Ysobelle M. Secreto to project header comment for documentation.

--- src/main/java/com/mycompany/motorph/MotorPH.java
+++ src/main/java/com/mycompany/motorph/MotorPH.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-// Bianca Ysobelle M. Secreto - name 
+// Bianca Ysobelle M. Secreto - name
 // lrbysecreto - GitHub username
 package com.mycompany.motorph;
 
